fee20b0df76da66de9b4b04bdd918b44fb816d04
sleepingxixi <liping242302@163.com>
2019-01-21 20:52:56 +0800

    add

--- slide/demo3-v2/js/demo3-v2.js
+++ slide/demo3-v2/js/demo3-v2.js
@@ -1,10 +1,17 @@
+// 这是轮播图升级版2，主要改进是：
+// 1.轮播图实现方式修改。从之前的复制一套图片的方式转换成仅用一套图片显示。
+// 2.优化了html结构，将图片与展示信息放在一个div中，结合相对定位和绝对定位实现。并且优化展示的函数。
+// 3.将图片和相关信息的数据进行了处理，变成了数组对象。
+// 4.使用了函数进行封装。
+// 5.改进用户指定显示的首张图片的方法。去掉排序，直接将index设置为用户需要看到的首张图片，并且指定了首张图片显示的位置
+
 $.fn.slide = function (options) {
     // 默认参数
     var defaults = {
         // 自动滚动开关
         autoSlide: "true",
-        // 轮播滚动的类型选择
-        slideType: "rolling",
+        // 轮播滚动的类型选择,rolling为左右滚动，fade为渐变
+        slideType: "crolling",
         // 导航按钮的样式,rectangle为长方形，circle为圆形,square为正方形
         slideNavType: "circle",
 
@@ -21,24 +28,28 @@ $.fn.slide = function (options) {
         picInfoFontFamily: "微软雅黑",
         picInfoFontSize: "14px",
         picInfoHight: "40px",
-        picInfoWidth: "100%",
+        picInfoWidth: "1000px",
         picInfoBackgroupColor: "rgba(97, 95, 95, 0.5)",
-        picInfoTop: "",
-        picInfoRight: "",
-        picInfoBottom: "0",
-        picInfoLeft: "0",
         picInfoPadding: "10px",
         picInfoColor: "black",
-        
-
 
         // 轮播间隔时间
         time: 1000,
         // 轮播速度
-        slideSpeed:500,
-        // 图片地址和描述
-        pic_url: ["img/ad2.jpg", "img/ad3.jpg", "img/ad4.jpg"],
-        pic_info: ["图片1", "图片2", "图片3"],
+        slideSpeed: 500,
+        pic_content: [
+            {
+                pic_url: "img/ad2.jpg",
+                pic_info: "图片1"
+            },
+            {
+                pic_url: "img/ad3.jpg",
+                pic_info: "图片2"
+            },
+            {
+                pic_url: "img/ad4.jpg",
+                pic_info: "图片3"
+            }],
         // 默认显示首张图片的问题，要求从0开始计算
         firstPicIndex: 2,
 
@@ -57,23 +68,21 @@ $.fn.slide = function (options) {
         var slideContent = null;
         var slideNavLi = null;
         var slideInfo = null;
-        var slideItem=null;
-        var slideImg = null;
+        var slideItem = null;
         var slideNav = null;
 
         var timer = null;   //定时器
         var time = options.time;    //轮播图切换事件(毫秒)
-        var slideSpeed=options.slideSpeed;
+        var slideSpeed = options.slideSpeed;
         var slideWidth = options.slideWidth; //显示窗口宽度
         var slideHeight = options.slideHeight; //显示窗口的高度
 
-        var pic_url = options.pic_url;
-        var pic_num = pic_url.length;
-        var pic_info = options.pic_info;
-        var oldLength = pic_num;    //item初始长度
-        var length = oldLength * 2;
-        var index = oldLength;  //当前索引值
+        var pic = options.pic_content;
+        var pic_num = pic.length;
+
+        var length = pic_num;    //item初始长度
         var firstPic = options.firstPicIndex;
+        var index = firstPic;  //当前索引值
 
         var slideNavType = options.slideNavType;
 
@@ -81,8 +90,6 @@ $.fn.slide = function (options) {
 
         //初始化
         function init() {
-            // 将图片的顺序重新排列
-            sortFirstPic();
 
             // 假定用户已经创建好slide显示窗口
             createContain();
@@ -90,72 +97,21 @@ $.fn.slide = function (options) {
             //添加轮播图的样式
             addSlideStyle();
 
-            //将item复制一份加入到原item的后面，形成:原1、原2、原3、原4、...原末、复1、复2、复3、复4...复末,并定位到复1项
-            slideContent.append(slideContent.html()).css({ "width": slideWidth * length + "px", "left": -slideWidth * index + "px" });
-
             //鼠标悬浮事件
-            slideEle.hover(function () {  //移除定时任务
-                clearInterval(timer);
-                //设置按钮的显示
-                slideEle.find('.slide-btn').css({ "display": "block" });
-            }, function () {   //添加定时任务    
-                isSetTimer();
-                slideEle.find('.slide-btn').css({ "display": "none" });
-            });
+            mouseoverEvent();
 
             //按钮点击事件
-            slideEle.find('.prev').click(function () {
-                if (!slideContent.is(':animated')) {
-                    index--;
-                    change();
-                }
+            buttonClickEvent();
 
-            }).end()
-                .find('.next').click(function () {
-                    if (!slideContent.is(':animated')) {
-                        index++;
-                        change();
-                    }
-                });
 
             //导航点点击事件委托
-            slideNavLi.click(function (event) {
-                // event.target用来说明最初触发事件的dom元素
-                index = $(event.target).index() + oldLength;
-                change();
-            });
+            navLiEvent();
 
+            // 用于判读用户是否需要自动轮播
             isSetTimer();
         }
 
-        function sortFirstPic() {
-            pic_url = sortPicAndInfo(firstPic, pic_url, pic_num);
-            pic_info = sortPicAndInfo(firstPic, pic_info, pic_num);
 
-        }
-
-        // 根据用户要求首张显示图片，对图片和信息进行排序
-        function sortPicAndInfo(firstPic, beSort, pic_num) {
-            var tempArr1 = [];
-            var tempArr2 = [];
-            if (firstPic > pic_num - 1 || firstPic < 0) {
-                //待处理
-
-            } else if (firstPic != 0) {
-                for (var j = 0; j < pic_num; j++) {
-                    if (j >= firstPic) {
-                        tempArr1.push(beSort[j]);
-                    } else {
-                        tempArr2.push(beSort[j]);
-                    }
-                }
-                tempArr1.push.apply(tempArr1, tempArr2);
-                beSort = tempArr1;
-                tempArr1 = null;
-                tempArr2 = null;
-                return beSort;
-            }
-        }
         // 创建轮播图的结构
         function createContain() {
             slideDiv.append('<div class="slide"></div>');
@@ -165,7 +121,7 @@ $.fn.slide = function (options) {
             //创建导航
             slideEle.append('<ul class="slide-nav"></ul>');
             //创建描述信息
-            slideEle.append('<div class="slide-info"><p></p></div>')
+            // slideEle.append('<div class="slide-info"><p></p></div>')
             //添加左右的箭头
             slideEle.append('<a href="javascript:void(0);" class="slide-btn prev"></a><a href = "javascript:void(0);" class= "slide-btn next" ></a>');
 
@@ -173,23 +129,23 @@ $.fn.slide = function (options) {
 
             slideNav = slideEle.find(".slide-nav");
             for (var pic_index = 0; pic_index < pic_num; pic_index++) {
-                slideContent.append('<div class="slide-item"><img src="' + pic_url[pic_index] + '" alt="' + pic_info[pic_index] + '"></div>');
+                slideContent.append('<div class="slide-item">'
+                    + '<img src="' + pic[pic_index].pic_url + '" alt="' + pic[pic_index].pic_url + '">'
+                    + '<span class="slide-info"><p>' + pic[pic_index].pic_info + '</p></span>'
+                    + '</div>');
                 slideNav.append('<li></li>');
             }
-            
+
             // 给第一个按钮添加样式
             slideNavLi = slideEle.find('.slide-nav li');
-            slideNavLi.first().addClass('active').css({
+            slideNavLi.eq(index).addClass('active').css({
                 "background-color": options.slideNavActiveColor,
                 "border-color": options.slideNavActiveColor
             });
 
-            slideInfo = slideEle.find('.slide-info');
-            // 初始默认的信息
-            slideInfo.find("p").html(pic_info[0]);
-            slideItem=slideEle.find('.slide-item');
+            slideItem = slideEle.find('.slide-item');
             slideImg = slideImg = slideEle.find('.slide-item img');
-
+            slideInfo = slideItem.find('.slide-info');
         }
 
         function addSlideStyle() {
@@ -203,15 +159,17 @@ $.fn.slide = function (options) {
                 slideHeight = slideDiv.height();
             }
             slideEle.css({
-                "width": slideWidth+"px",
-                "height": slideHeight+"px"
-            });
-            slideItem.css({
                 "width": slideWidth + "px",
                 "height": slideHeight + "px"
             });
-            console.log(slideWidth, slideHeight)
-
+            slideContent.css({
+                "width": slideWidth * length + "px",
+                // "left": -slideWidth * index + "px"
+            }).css(leftForIndex(index));
+            slideItem.css({
+                "width": slideWidth + "px",
+                "height": slideHeight + "px",
+            });
 
             // 设置导航样式
             // 设置导航形状
@@ -250,7 +208,7 @@ $.fn.slide = function (options) {
                 "right": options.picInfoRight,
                 "background-color": options.picInfoBackgroupColor,
                 "height": options.picInfoHight,
-                "width":options.picInfoWidth
+                "width": options.picInfoWidth
             });
             slideInfo.find("p").css({
                 "color": options.picInfoColor,
@@ -258,11 +216,47 @@ $.fn.slide = function (options) {
                 "font-size": options.picInfoFontSize,
                 "padding": options.picInfoPadding
             })
-
-
         }
 
-        // 用于判读用户是否需要轮播
+        // 鼠标悬停事件，用于设置鼠标停在轮播图上时候的定时及按钮的状态
+        function mouseoverEvent() {
+            slideEle.hover(function () {  //移除定时任务
+                clearInterval(timer);
+                //设置按钮的显示
+                slideEle.find('.slide-btn').css({ "display": "block" });
+            }, function () {   //添加定时任务    
+                isSetTimer();
+                slideEle.find('.slide-btn').css({ "display": "none" });
+            });
+        }
+
+        // 按钮点击事件，用户触发按钮事件
+        function buttonClickEvent() {
+            slideEle.find('.prev').click(function () {
+                if (!slideContent.is(':animated')) {
+                    index--;
+                    change();
+                }
+
+            }).end()
+                .find('.next').click(function () {
+                    if (!slideContent.is(':animated')) {
+                        index++;
+                        change();
+                    }
+                });
+        }
+
+        // 设置导航按钮的事件
+        function navLiEvent() {
+            slideNavLi.click(function (event) {
+                // event.target用来说明最初触发事件的dom元素
+                index = $(event.target).index();
+                change();
+            });
+        }
+
+        // 用于判读用户是否需要自动轮播
         function isSetTimer() {
             if (options.autoSlide == "true") {
                 setTimer();
@@ -277,26 +271,59 @@ $.fn.slide = function (options) {
             }, time);
         }
 
+        // 给用户设置动画
         function change() {
             changeSlide();
             changeNav();
-            changeInfo();
+            // changeInfo();
         }
 
         //轮播图切换
         function changeSlide() {
-            slideContent.animate({ left: -slideWidth * index + "px" }, slideSpeed, function () {
-                if (index <= 0) {
-                    //当定位到原1时，在回调函数中将slideContent瞬间定位到复1
-                    index = oldLength;
-                    slideContent.css({ left: -slideWidth * index + "px" });
-                }
-                if (index >= length - 1) {
-                    //当定位到复末时，在回调中将slideContent瞬间定位到原末
-                    index = oldLength - 1;
-                    slideContent.css({ left: -slideWidth * index + "px" });
-                }
-            });
+            switch (options.slideType) {
+                case "fade":
+                    fade();
+                    break;
+                default:
+                    rolling();
+                    break;
+            }
+        }
+
+        // 设置滚动的函数
+        function rolling() {
+            console.log(slideSpeed);
+            console.log("oldIndex" + index);
+            if (index < 0) {
+                index = length - 1;
+                // slideContent.css({ left: -slideWidth * (index - 1) + "px" });
+
+                // 当需要跳转到最后一张的时候会先让其快速切换到倒数第二张
+                rollingToIndex(index - 1);
+            } else if (index >= length) {
+                index = 0;
+                // slideContent.css({ left: -slideWidth * (index + 1) + "px" });
+
+                // 当需要其跳转到第1张的时候，会快速让其先切换到第二张
+                rollingToIndex(index + 1);
+            }
+            slideContent.animate(leftForIndex(index), slideSpeed);
+        }
+
+        // 用于设置滚动框的位置
+        function rollingToIndex(goalIndex) {
+            slideContent.css(leftForIndex(goalIndex));
+        }
+
+        // 用于设置向左滚动的位置
+        function leftForIndex(goalIndex) {
+            return { "left": -slideWidth * goalIndex + "px" };
+        }
+
+        function fade() {
+            slideItem.css({
+                "float": "left"
+            })
         }
         //导航点切换
         function changeNav() {
@@ -304,19 +331,19 @@ $.fn.slide = function (options) {
                 "background-color": "",
                 "border-color": options.slideNavColor
             })
-                .eq(index % oldLength).addClass('active').css({
+                .eq(index % length).addClass('active').css({
                     "background-color": options.slideNavActiveColor,
                     "border-color": options.slideNavActiveColor
                 });
         }
 
         // 切换描述信息
-        function changeInfo() {
-            var info = $(slideImg.get(index % oldLength)).attr('alt');
-            slideInfo.html(function () {
-                return "<p>" + info + "</p>";
-            });
-        }
+        // function changeInfo() {
+        //     var info = $(slideImg.get(index)).attr('alt');
+        //     slideInfo.html(function () {
+        //         return "<p>" + info + "</p>";
+        //     });
+        // }
 
     });
     return this;
